f7e897ab985991d3c10be5000ff54321bc61c22a
jhawlc <j.hanke@wlc.ac.uk>
2023-04-13 16:53:55 +0100

    Added controls for the forms

--- FilmQuiz/frmMain.Designer.vb
+++ FilmQuiz/frmMain.Designer.vb
@@ -22,15 +22,34 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
+        'btnStart
+        '
+        Me.btnStart.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnStart.Location = New System.Drawing.Point(635, 354)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(111, 44)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "START"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(162, 340)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 1
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(596, 372)
+        Me.Button1.Location = New System.Drawing.Point(22, 107)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
+        Me.Button1.Size = New System.Drawing.Size(119, 33)
+        Me.Button1.TabIndex = 2
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
@@ -40,11 +59,16 @@ Partial Class frmMain
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnStart)
         Me.Name = "frmMain"
         Me.Text = "frmMain"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnStart As Button
+    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
 End Class
